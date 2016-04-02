ca67ed1e822df2b6c03f7b71f170161c775f0ded
ZakariyyaSv <1248800415@qq.com>
2016-04-02 09:21:39 +0800

    Initial Commit

---
+++ HNQQPopMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HNQQPopMenu
+//
+//  Created by 许浩男 on 16/4/2.
+//  Copyright © 2016年 Zakariyya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ HNQQPopMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HNQQPopMenu
+//
+//  Created by 许浩男 on 16/4/2.
+//  Copyright © 2016年 Zakariyya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
